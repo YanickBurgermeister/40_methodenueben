5fcb57ddf3540a4a8cf3f6b3032f5426a698c855
Yanick <yanick.burgermeister@gmail.com>
2023-01-10 11:03:02 +0100

    3 Vertiefungsaufgaben

--- src/main/java/ch/bbw/pr/methoden/App.java
+++ src/main/java/ch/bbw/pr/methoden/App.java
@@ -23,16 +23,16 @@ public class App
         System.out.println( "\nVertiefungsübung Methoden Teil 2" );
         Auto myAuto = new Auto();
         int ps = myAuto.changeKilowattToPS(3443);
-
+        System.out.println(ps);
 
 
         //Vertiefungsübung Methoden Teil 3
         // Aufruf der Methoden zur Klasse Menschen
         System.out.println( "\nVertiefungsübung Methoden Teil 3" );
         Menschen mensch = new Menschen();
-        mensch.getNamePlusAlter("Paul", 15);
-        mensch.bevoelkerungsAnteil(15000, 0.5);
-        mensch.ausbreitung(500, true);
+        System.out.println(mensch.getNamePlusAlter("Paul", 15));
+        System.out.println(mensch.bevoelkerungsAnteil(15000, 0.5));
+        System.out.println(mensch.ausbreitung(500, true));
 
     }
 }
